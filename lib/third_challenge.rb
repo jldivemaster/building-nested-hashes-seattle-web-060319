9d72fd893122d8c2da99d31c153ac174edf1d722
Jason Leach <jlspursfan@gmail.com>
2019-05-17 19:53:26 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -21,7 +21,7 @@ def third_challenge
    },
    :capulet => {
       :patriarch => {
-        :name, "Lord Capulet",
+        :name => "Lord Capulet",
         :age => "50"
         #your key/value pairs here
         },
